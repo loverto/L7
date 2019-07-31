6a2ad033b55b1286958450a74939a5fe2a525cfb
ylf <1045438139@qq.com>
2019-07-31 21:11:14 +0800

    添加glsl 动画部分的说明

--- module/glsl/defaultlinefs.glsl
+++ module/glsl/defaultlinefs.glsl
@@ -4,13 +4,17 @@ varying vec4 v_color;
 varying float vTime;
 void main() {
     vec4 color = v_color;
-
+    // 设置动画
     #ifdef ANIMATE
+    // 如果时间大于1.0 或小于0则结束
     if (vTime > 1.0 || vTime < 0.0) {
         discard;
     }
+    // 设置颜色的透明度
     color.a= color.a * vTime * 1.5;
     #endif
+    // gl Frag Color
     gl_FragColor = color;
+    // 设置颜色的透明度
     gl_FragColor.a =color.a*u_opacity;
 }
--- module/glsl/defaultlinevs.glsl
+++ module/glsl/defaultlinevs.glsl
@@ -5,10 +5,17 @@ uniform float u_time;
 varying float vTime;
 varying vec4 v_color;
 void main() {
+    // mat模型视图投影
+    // 投影矩阵
+    // 模型查看矩阵
     mat4 matModelViewProjection = projectionMatrix * modelViewMatrix;
+    // 设置颜色
     v_color = a_color;
+    // 如果动画开启
     #ifdef ANIMATE
+    // 当前时间
     vTime = 1.0- (mod(u_time*50.,3600.)- position.z) / 100.;
     #endif
+    // gl位置
     gl_Position = matModelViewProjection * vec4(position.xy,0., 1.0);
 }
